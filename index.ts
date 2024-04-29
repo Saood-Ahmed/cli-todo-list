cac844885105b4e971c53a604dc4d7c03cb63d13
Saood-Ahmed <unknownsuperhero@yahoo.com>
2024-04-29 16:06:35 +0500

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+
+let todos = [];
+let condition = true;
+
+console.log("\n\t Welcome to Saood's Todo List! \t\n");
+
+while(condition) {
+    let addTodo = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "Enter your Todo"
+        }
+    ]);
+
+    todos.push(addTodo.todo);
+    console.log(`${addTodo.todo} Task added in Todo-List Successfully!`);
+
+    let addMoreTodos = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more Todos?",
+            default: "False"
+        }
+    ]);
+    condition = addMoreTodos.addMore;
+}
+
+console.log(`Your Updated Todo-List: ${todos}`);
